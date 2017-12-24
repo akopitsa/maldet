87499144db6d36bd2745a88149a4f8f69594ff51
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 20:21:03 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -16,7 +16,7 @@ class maldet::install inherits maldet  {
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
-    require => File['/tmp/maldetect-current.tar.gz'],
+    require => File['maldetfile'],
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
     alias   => 'extractmaldet',
   }
