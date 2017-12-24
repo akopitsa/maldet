f61acd88b42b73f3aa696043a6b9417295033782
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 21:53:50 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,19 +6,20 @@ class maldet::install inherits maldet  {
     alias   => 'maldetlatest',
     cwd     => '/tmp',
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    notice("Download"),
+    notify { 'download': } 
+    
   }
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    notice("extract"),
+    
   }
   exec { 'install-maldet':
     command => "/tmp/maldetect-$version/install.sh",
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
     cwd     => "/tmp/maldetect-$version",
-    notice("install"),
+    
   }
   file {'/usr/local/maldetect/conf.maldet':
     ensure => file,
@@ -27,12 +28,12 @@ class maldet::install inherits maldet  {
     mode   => '0644',
     content => template("maldet/conf.maldet.erb"),
     require => Exec['install-maldet'],
-    notice("config"),
+    
   }
   file {'/tmp/maldetect-current.tar.gz':
     ensure  => absent,
     source  => '/tmp/maldetect-current.tar.gz',
     require => File['/usr/local/maldetect/conf.maldet'],
-    notice("remove"),
+    
   }  
 }
