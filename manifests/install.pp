e959c667295a76051cb58655ae526e84c9abee0e
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 20:42:51 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,20 +10,18 @@ class maldet::install inherits maldet  {
   file {'/tmp/maldetect-current.tar.gz':
     ensure  => file,
     source  => '/tmp/maldetect-current.tar.gz',
-    alias   => 'maldetfile',
+#    alias   => 'maldetfile',
   }
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
     require => File['maldetfile'],
-    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    
+    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],    
   }
   exec { 'install-maldet':
     command => "/tmp/maldetect-$version/install.sh",
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    cwd     => "/tmp/maldetect-$version",
-    
+    cwd     => "/tmp/maldetect-$version",  
   }
   file {'/usr/local/maldetect/conf.maldet':
     ensure => file,
@@ -31,15 +29,15 @@ class maldet::install inherits maldet  {
     group  => root,
     mode   => '0644',
     content => template("maldet/conf.maldet.erb"),
-    alias => 'configfile',
+#    alias => 'configfile',
   }
-  file {"/tmp/maldetect-$version":
-    ensure => absent,
-    path => "/tmp/maldetect-$version",
-    recurse => true,
-    purge => true,
-    force => true,
-    require => File['configfile'],
-  }
+  # file {"/tmp/maldetect-$version":
+  #   ensure => absent,
+  #   path => "/tmp/maldetect-$version",
+  #   recurse => true,
+  #   purge => true,
+  #   force => true,
+  #   require => File['configfile'],
+  # }
   
 }
