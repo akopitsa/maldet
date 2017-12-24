188ce61b04f59ff58963b3470354e5836e99dfe1
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 18:27:24 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,12 +9,19 @@ class maldet::install inherits maldet {
   #   mode   => 0644,
   #   source => 'puppet:///modules/maldet/maldetect-current.tar.gz',
   # }
-  exec { 'download':
-    cwd     => "/tmp",
-    command => "/usr/bin/wget -q http://www.rfxn.com/downloads/maldetect-current.tar.gz -O /tmp/maldetect-current.tar.gz",
-#    require => File['/tmp/maldetect-current.tar.gz'],
-    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  file {'maldetect-current.tar.gz':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => 0644,
+    content => download_content('http://www.rfxn.com/downloads/maldetect-current.tar.gz'),
   }
+#   exec { 'download':
+#     cwd     => "/tmp",
+#     command => "/usr/bin/wget -q http://www.rfxn.com/downloads/maldetect-current.tar.gz -O /tmp/maldetect-current.tar.gz",
+# #    require => File['/tmp/maldetect-current.tar.gz'],
+#     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+#   }
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
