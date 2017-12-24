0fc0d9defa14ef365cf885d8ef549b8a7f80c52f
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 21:59:33 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,21 +5,20 @@ class maldet::install inherits maldet  {
   exec {'wget http://www.rfxn.com/downloads/maldetect-current.tar.gz':
     alias   => 'maldetlatest',
     cwd     => '/tmp',
-    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    notify { 'download': } 
-    
+    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],    
+  }
+  notify { 'Hello World':
+    require => Exec['maldetlatest'],
   }
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
-    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    
+    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],    
   }
   exec { 'install-maldet':
     command => "/tmp/maldetect-$version/install.sh",
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    cwd     => "/tmp/maldetect-$version",
-    
+    cwd     => "/tmp/maldetect-$version",    
   }
   file {'/usr/local/maldetect/conf.maldet':
     ensure => file,
@@ -27,13 +26,11 @@ class maldet::install inherits maldet  {
     group  => root,
     mode   => '0644',
     content => template("maldet/conf.maldet.erb"),
-    require => Exec['install-maldet'],
-    
+    require => Exec['install-maldet'],    
   }
   file {'/tmp/maldetect-current.tar.gz':
     ensure  => absent,
     source  => '/tmp/maldetect-current.tar.gz',
     require => File['/usr/local/maldetect/conf.maldet'],
-    
   }  
 }
