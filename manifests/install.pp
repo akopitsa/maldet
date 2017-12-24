44b8caf5fa6f567d86e1394e82858cb3bd05931c
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 21:50:20 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,19 +6,19 @@ class maldet::install inherits maldet  {
     alias   => 'maldetlatest',
     cwd     => '/tmp',
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    notify { 'download': notice("Download") },
+    notify { 'download': notice("Download"), },
   }
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-    notify { 'extract': notice("extract") },
+    notify { 'extract': notice("extract"), },
   }
   exec { 'install-maldet':
     command => "/tmp/maldetect-$version/install.sh",
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
     cwd     => "/tmp/maldetect-$version",
-    notify { 'install': notice("install") },
+    notify { 'install': notice("install"), },
   }
   file {'/usr/local/maldetect/conf.maldet':
     ensure => file,
@@ -27,12 +27,12 @@ class maldet::install inherits maldet  {
     mode   => '0644',
     content => template("maldet/conf.maldet.erb"),
     require => Exec['install-maldet'],
-    notify { 'config': notice("config") },
+    notify { 'config': notice("config"), },
   }
   file {'/tmp/maldetect-current.tar.gz':
     ensure  => absent,
     source  => '/tmp/maldetect-current.tar.gz',
     require => File['/usr/local/maldetect/conf.maldet'],
-    notify { 'remove': notice("remove") },
+    notify { 'remove': notice("remove"), },
   }  
 }
