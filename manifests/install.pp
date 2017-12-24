4ee5f87d86484c9dd9f442fc4a2aef7c90f34e26
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-24 18:51:46 +0200

    Add changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,33 +10,34 @@ class maldet::install inherits maldet {
   #   source => 'puppet:///modules/maldet/maldetect-current.tar.gz',
   # }
   exec {'wget http://www.rfxn.com/downloads/maldetect-current.tar.gz':
-    alias => 'maldetlatest',
-    cwd => '/tmp',
+    alias   => 'maldetlatest',
+    cwd     => '/tmp',
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
   }
   file {'/tmp/maldetect-current.tar.gz':
-    ensure => file,
-    source => '/tmp/maldetect-current.tar.gz',
-    alias => 'maldetfile',
+    ensure  => file,
+    source  => '/tmp/maldetect-current.tar.gz',
+    alias   => 'maldetfile',
     require => Exec['maldetlatest'],
   }
-#   exec { 'download':
-#     cwd     => "/tmp",
-#     command => "/usr/bin/wget -q http://www.rfxn.com/downloads/maldetect-current.tar.gz -O /tmp/maldetect-current.tar.gz",
-# #    require => File['/tmp/maldetect-current.tar.gz'],
-#     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-#   }
   exec { 'extract':
     cwd     => "/tmp",
     command => "tar -xzvf maldetect-current.tar.gz",
     require => File['/tmp/maldetect-current.tar.gz'],
     path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+    alias   => 'extractmaldet',
   }
-  # file {'/usr/local/maldetect/conf.maldet':
-  #   ensure => file,
-  #   owner  => root,
-  #   group  => root,
-  #   mode   => '0644',
-  #   content => template("maldet/conf.maldet.erb")
-  # }
+  exec { 'install-maldet':
+    command => "/tmp/maldet-*/install.sh",
+    path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+    cwd     => "/tmp/maldet-*",
+    require => Exec['extractmaldet'],
+  }
+  file {'/usr/local/maldetect/conf.maldet':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    content => template("maldet/conf.maldet.erb")
+  }
 }
