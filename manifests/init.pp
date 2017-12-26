27f4b5f48c3b6b2db185e838dcc42c57a25d361d
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-26 11:12:53 +0200

    Add changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,12 @@ class maldet (
   String $version     = $maldet::install::version,
   Boolean $email_alert = $maldet::config::email_alert,
 ){
-  contain maldet::install
-  contain maldet::config
-  contain maldet::postinstall
-
+  # contain maldet::install
+  # contain maldet::config
+  # contain maldet::postinstall
+  class { '::maldet::install': }
+  class { '::maldet::config': }
+  class { '::maldet::postinstall': }
   Class['::maldet::install']
   -> Class['::maldet::config']
   -> Class['::maldet::postinstall']
