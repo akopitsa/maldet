30458c47149d7e0ce5194b379740e673976253e6
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-26 12:06:48 +0200

    Add changes

--- manifests/postinstall.pp
+++ manifests/postinstall.pp
@@ -5,17 +5,18 @@ class maldet::postinstall (
   String $version = $maldet::version,
 ) inherits maldet {
   file {"/tmp/$version.tar.gz":
-    alias   => 'remove',
+    alias   => 'removearchive',
     ensure  => absent,
     source  => "/tmp/$version.tar.gz",
   }
-  # file {"removefolder":
-  #   ensure  => absent,
-  #   path    => "/tmp/maldetect-$version",
-  #   recurse => true,
-  #   purge   => true,
-  #   force   => true,
-  # }
+  file {"linux-malware-detect-$version":
+    ensure  => absent,
+    path    => "/tmp/linux-malware-detect-$version",
+    recurse => true,
+    purge   => true,
+    force   => true,
+    require => File['removearchive'],
+  }
   notify { "/tmp/maldetect-$version":
   message => "/tmp/maldetect-$version"
   }
